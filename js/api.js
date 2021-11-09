4e456627c7e681cdfa9f9d055ce19617ebddaa68
Kaiserlucas <32715218+Kaiserlucas@users.noreply.github.com>
2021-11-09 15:31:04 +0100

    Fixed trip deletion and added trip ID generation

--- js/api.js
+++ js/api.js
@@ -5,14 +5,24 @@ const getTrips = async () => {
 
 const saveTrip = async (trip) => {
     let oldTrips = await getTrips();
-    //TODO: We probably want to calculate the new ID here later
+
+    //Set id for trip
+    let ids = oldTrips.map(X=>X.id);
+    if(ids.length === 0) {
+        trip.id = 1;
+    } else {
+        trip.id = Math.max(...ids)+1
+    }
+
     oldTrips.push(trip);
     localStorage.setItem("trips",JSON.stringify(oldTrips))
 }
 
 const deleteTrip = async (id) =>{
     const oldTrips = await getTrips();
-    const newTrips = oldTrips.filter(X=>X.id === id);
+    console.log(id);
+    console.log(oldTrips)
+    const newTrips = oldTrips.filter(X=>X.id !== parseInt(id));
     localStorage.setItem("trips",JSON.stringify(newTrips))
 }
 
