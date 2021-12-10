13005d0fa871c5724575d31fde71fe12aee2450f
Ostervogel <m.lozahenrich@gmail.com>
2021-12-10 17:14:20 +0100

    Testing frontend-backend api communication

--- js/login.js
+++ js/login.js
@@ -1,15 +1,31 @@
-
 const loginForm = document.querySelector('#loginForm');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 
-const wantedEmail = "huehne@htw-berlin.de";
-const wantedPassword = "hunter2";
+/*const wantedEmail = 'huehne@htw-berlin.de';
+const wantedPassword = 'hunter2';*/
 
 loginForm.addEventListener('submit', () => {
-    if(email.value === wantedEmail && password.value === wantedPassword) {
-        window.location.href = "karte.html";
-    } else {
-        alert("Falscher Benutzername oder Password.");
-    }
+  const loginInformation = { email: email.value, password: password.value };
+
+  const fetchParams = {
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: loginInformation,
+    method: 'POST',
+    mode: 'cors',
+  };
+  fetch('https://webdevelopment-travelsite.herokuapp.com/login', fetchParams)
+    .then((data) => {
+      return data.json();
+    })
+    .then((res) => console.log(res))
+    .catch((error) => console.log(error));
+
+  /*if (email.value === wantedEmail && password.value === wantedPassword) {
+    window.location.href = 'karte.html';
+  } else {
+    alert('Falscher Benutzername oder Password.');
+  }*/
 });
