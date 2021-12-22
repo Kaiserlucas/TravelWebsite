125077cb421f33ba8ba0ba1d7a273525e968b80b
Kaiserlucas <32715218+Kaiserlucas@users.noreply.github.com>
2021-12-22 20:55:26 +0100

    Added basic signup page

--- js/login.js
+++ js/login.js
@@ -2,15 +2,9 @@ const loginForm = document.querySelector('#loginForm');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 
-/*const wantedEmail = 'huehne@htw-berlin.de';
-const wantedPassword = 'hunter2';*/
-//const toUrlEncoded = obj => Object.keys(obj).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(obj[k])).join('&');
-
 loginForm.addEventListener('submit', () => {
   const loginInformation = { email: email.value, password: password.value };
- // const encoded = toUrlEncoded(loginInformation);
   const json = JSON.stringify(loginInformation);
- // console.log(encoded);
   const fetchParams = {
     headers: {
       'content-type': 'application/json',
@@ -37,11 +31,4 @@ loginForm.addEventListener('submit', () => {
         handleResponse(res))
     .catch((error) =>
         console.log(error))
-
-
-  /*if (email.value === wantedEmail && password.value === wantedPassword) {
-    window.location.href = 'karte.html';
-  } else {
-    alert('Falscher Benutzername oder Password.');
-  }*/
 });
---
+++ js/signup.js
@@ -0,0 +1,63 @@
+const loginForm = document.querySelector('#loginForm');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const retypePassword = document.querySelector('#password-retype');
+
+loginForm.addEventListener('submit', () => {
+    if(password.value === retypePassword.value) {
+    const signupInformation = { email: email.value, password: password.value };
+    const json = JSON.stringify(signupInformation);
+    const fetchParams = {
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: json,
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+    };
+
+    function handleResponse(res) {
+        if(res.message === 'Bad email or password') {
+            alert('Serverseitiger Fehler. Bitte versuchen Sie es erneut.');
+        } else {
+            changePageContentAfterSignup()
+        }
+    }
+
+    function changePageContentAfterSignup() {
+        const div = document.querySelector('#div');
+        email.remove()
+        password.remove()
+        retypePassword.remove()
+
+        const header = document.createElement('h3');
+        header.innerText = 'Registrierung abgeschickt.';
+        div.appendChild(header);
+
+        const text = document.createElement('p');
+        text.innerText = 'Bitte bestätigen Sie Ihr Konto indem sie dem Link in der soeben versendeten Bestätigungsemail folgen.';
+        div.appendChild(text);
+
+        const link = document.createElement('a');
+        link.innerText = 'Zurück zum Login';
+        link.setAttribute('href','login.html')
+        div.appendChild(link);
+    }
+
+    /*
+    fetch('https://webdevelopment-travelsite.herokuapp.com/signup', fetchParams)
+        .then((data) => {
+            return data.json();
+        })
+        .then((res) =>
+            handleResponse(res))
+        .catch((error) =>
+            console.log(error))
+     */
+        changePageContentAfterSignup()
+
+    } else {
+        alert('Passwörter sind nicht gleich.');
+    }
+});
