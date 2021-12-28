f1571c4f896f9a3583cccce7a9b46aa778e45b66
Kaiserlucas <32715218+Kaiserlucas@users.noreply.github.com>
2021-12-28 23:39:46 +0100

    Added event listener for map popup submissions

--- js/map.js
+++ js/map.js
@@ -1,56 +1,60 @@
-import { getTrips, getJson } from './api.js';
+import {getTrips, getJson, saveTrip} from './api.js';
+import Trip from "./Trip";
 
 const popupForm = document.getElementById("popupForm");
 const name = document.getElementById("name");
 const destination = document.getElementById("destination");
 const startdate = document.getElementById("startdate");
 const enddate = document.getElementById("enddate");
+const saveButton = document.getElementById("saveButton");
 
-const visitedCountries = async () => {
+const init = async () => {
+  const visitedCountries = async () => {
 
-  const countriePromises = await getTrips();
-  if(countriePromises.message === 'You need to be logged in to see this page.') {
-    window.location.href = 'login.html';
-  }
-  const countries = [];
-  for (const [i, trip] of countriePromises.entries()) {
-    countries[i] = trip.destination;
-  }
-  return countries;
-};
-
-const countries = await visitedCountries();
-
-const data = await getJson('../resources/countries.geojson');
-const map = L.map('map').setView([0, 0], 3);
-L.geoJSON(data, {
-  clickable: true,
-  stroke: false,
-  fill: true,
-  fillColor: '#B0B0B0',
-  fillOpacity: 1,
-  onEachFeature: onEachFeature,
-  style: function (feature) {
-    for (const country of countries) {
-      switch (feature.properties.ADMIN) {
-        case country:
-          return {
-            fill: false,
-          };
-        case "Antarctica":
-          return {
-            fill: false,
-          };
-      }
+    const countriePromises = await getTrips();
+    if(countriePromises.message === 'You need to be logged in to see this page.') {
+      window.location.href = 'login.html';
     }
-  },
-}).addTo(map);
+    const countries = [];
+    for (const [i, trip] of countriePromises.entries()) {
+      countries[i] = trip.destination;
+    }
+    return countries;
+  };
 
-let osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  noWrap: true,
-});
-osm.addTo(map);
+  const countries = await visitedCountries();
+
+  const data = await getJson('../resources/countries.geojson');
+  const map = L.map('map').setView([0, 0], 3);
+  L.geoJSON(data, {
+    clickable: true,
+    stroke: false,
+    fill: true,
+    fillColor: '#B0B0B0',
+    fillOpacity: 1,
+    onEachFeature: onEachFeature,
+    style: function (feature) {
+      for (const country of countries) {
+        switch (feature.properties.ADMIN) {
+          case country:
+            return {
+              fill: false,
+            };
+          case "Antarctica":
+            return {
+              fill: false,
+            };
+        }
+      }
+    },
+  }).addTo(map);
+
+  let osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    noWrap: true,
+  });
+  osm.addTo(map);
+}
 
 function click(e) {
   const country = e.target.feature.properties.ADMIN;
@@ -69,3 +73,18 @@ function onEachFeature(feature, layer) {
     click: click,
   });
 }
+
+saveButton.addEventListener('submit', () => {
+  const trip = new Trip(
+      1,
+      name.value,
+      destination.value,
+      startdate.value,
+      enddate.value
+  );
+  saveTrip(trip).then(() => {
+    setTimeout(init,2000)
+  });
+});
+
+await init();
