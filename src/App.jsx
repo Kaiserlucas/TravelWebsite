1851af26c88348eddee46ad5ea9bcd7dfb6c1ad3
Ostervogel <m.lozahenrich@gmail.com>
2022-01-19 16:41:20 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Login from './components/pages/Login/Login';
 import Karte from './components/pages/Karte/Karte';
 import Reisen from './components/pages/Reisen/reisen';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Redirect } from 'react-router';
+import { Navigate } from 'react-router';
 
 function App() {
   
@@ -11,10 +11,10 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Redirect exact from="/" to="/login" />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/karte" element={<Karte/>}/>
-          <Route path="/reisen" element={<Reisen/>}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate replace to="/login"/>} />
+          <Route path="/karte" element={<Karte />} />
+          <Route path="/reisen" element={<Reisen />} />
         </Routes>
       </Router>
     </>
