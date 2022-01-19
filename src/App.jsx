fc29c83d21f02f2a76197d3ba57e4e1582455c24
Ostervogel <m.lozahenrich@gmail.com>
2022-01-19 16:37:05 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import './App.css';
 import Login from './components/pages/Login/Login';
 import Karte from './components/pages/Karte/Karte';
 import Reisen from './components/pages/Reisen/reisen';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   
   return (
     <>
       <Router>
-        <Switch>
+        <Routes>
           <Redirect exact from="/" to="/login" />
           <Route path="/login">
             <Login />
@@ -20,7 +20,7 @@ function App() {
           <Route path="/reisen">
             <Reisen />
           </Route>
-        </Switch>
+        </Routes>
       </Router>
     </>
   );
