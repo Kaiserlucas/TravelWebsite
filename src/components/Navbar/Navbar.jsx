2682683df2fc3aceb145d3cbcc84255e1ce75ee5
Ostervogel <m.lozahenrich@gmail.com>
2022-01-24 00:23:08 +0100

    fix bearbeiten der reisen

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import './style.css'
+import React from 'react';
+import './style.css';
 
 export default function Navbar() {
-    return (
-      <div className="navbar">
-        <ul className="navlist">
-          <li className="nav-item">
-            <a href="karte">
-              <img id="logo" src="../../ressources/logo.png" alt="logo" />
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="karte">Startseite</a>
-          </li>
-          <li className="nav-item">
-            <a href="reisen">Reisen</a>
-          </li>
-          <li className="nav-item">
-            <form action="login">
-              <button>Logout</button>
-            </form>
-          </li>
-        </ul>
-      </div>
-    );
+  return (
+    <div className="navbar">
+      <ul className="navlist">
+        <li className="nav-item">
+          <a href="karte">
+            <img id="logo" src="../../ressources/logo.png" alt="logo" />
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="karte">Startseite</a>
+        </li>
+        <li className="nav-item">
+          <a href="reisen">Reisen</a>
+        </li>
+        <li className="nav-item">
+          <form action="login">
+            <button>Logout</button>
+          </form>
+        </li>
+      </ul>
+    </div>
+  );
 }
